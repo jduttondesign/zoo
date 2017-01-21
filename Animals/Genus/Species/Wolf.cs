afe3854a8e6d990d4221685fe9f8546838334106
Jennifer Dutton <jenniferdutton@Jennifers-MacBook-Pro.local>
2017-01-20 20:51:33 -0600

    gitignore

---
+++ Animals/Genus/Species/Wolf.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Zoolandia.Animals.Canis.Wolf
+{ 
+    public class Wolf: Canis
+    {
+        public Wolf ()
+        {
+            this.name = "Fred";
+        }
+        public Wolf(string name)
+        {
+            this.name = name;
+        }
+    }
+}    
+            
+        
+        
+
+    }       
+        public Wolf()
+}
+
